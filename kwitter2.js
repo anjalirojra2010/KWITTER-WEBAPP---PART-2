6885f132456232b1e74411b90788504103ad4903
anjalirojra2010 <89329626+anjalirojra2010@users.noreply.github.com>
2021-08-28 13:47:04 +0530

    Update kwitter2.js

--- kwitter2.js
+++ kwitter2.js
@@ -1,11 +1,25 @@
-function redirectToRoomName(name)
-{
-    console.log(name);
-   localStorage.setItem("room_name" , name);
-    window.location = "kwitter_page.html";
+function getData()  
+{  
+        firebase.database().ref("/"+room_name).on('value', function(snapshot) 
+        { 
+                document.getElementById("output")
+                firebase_message_id = childKey;
+                message_data = chilData;
+                //Start code
+                console.log(firebase_message_id);
+                console.log(message_data);
+                name = message_data['name'];
+                message = message_data['message'];
+                like = message_data['like'];
+                name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+                message_with_tag = "<h4 class= 'message_h4'>" + message + "</h4>"
+                like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updatelike(this.id)'>";
+                span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+        
+                row = name_with_tag + message_with_tag +like_button + span_with_tag;
+                document.getElementById("output").innerHTML += row;
+                //End code
+        });
 }
-  function logout(){
-   localStorage.removeItem("user_name");
-   localStorage.removeItem("room_name");
-     window.location = "kwitter.html";
-  }
+getData();
+ 
